d6da9f6dc9ad80066e70c5e3eaaadad67694f18e
Maxwell DeMaio <maxdman321@gmail.com>
2022-03-24 00:22:45 -0400

    finish skeleton for parse/codewriting

--- vmTranslator/src/main/java/VmTranslator.java
+++ vmTranslator/src/main/java/VmTranslator.java
@@ -1,9 +1,20 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class VmTranslator {
     public static void main(String[] args) throws FileNotFoundException {
+        // We should use the cmd line arg
+        // if input is dir, we process all .vm files in dir
+        // if input is *.vm we just translate that one file
+
         FileInputStream vmLangInput = new FileInputStream("");
+        FileOutputStream assemblyOutput = new FileOutputStream("");
         Parser myParser = new Parser(vmLangInput);
+        CodeWriter myCodeWriter = new CodeWriter(assemblyOutput);
+
+        // while parser hasNext, we march through VM commands
+        // generate assembly code for each VM command (several assembly commands)
+
     }
 }
