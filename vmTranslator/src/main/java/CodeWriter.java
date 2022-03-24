cff90ef2247cfabfa3a0d948fad549918396a74f
Maxwell DeMaio <maxdman321@gmail.com>
2022-03-24 00:19:35 -0400

    make skeleton for codewriter class

--- vmTranslator/src/main/java/CodeWriter.java
+++ vmTranslator/src/main/java/CodeWriter.java
@@ -1,2 +1,29 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class CodeWriter {
+    FileOutputStream output;
+
+    CodeWriter(FileOutputStream output) {
+        this.output = output;
+    }
+
+    void setFileName(String fileName) {
+        // inform that the translation of new vm file has started
+        System.out.println("Starting translation of VM program " + fileName + "!");
+        System.out.println();
+    }
+
+    void writeArithmetic(String command) {
+        // Write the assembly code that is the translation of arith cmd
+    }
+
+    void writePushPop(String command, int index) {
+        // write assembly code that is the translation of given command
+        // where command is either c_push/pop
+    }
+
+    void close() throws IOException {
+        this.output.close();
+    }
 }
